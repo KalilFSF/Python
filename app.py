434bf43437994e115bd57cdea30516142c93f969
KALIL FELIPE <kalil.fsf@gmail.com>
2026-05-22 21:04:38 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+<!--Esse template herda no extends o layout.html
+ELe tem estruturas básicas de layout-->
+
+{% extends 'layout.html' %}
+
+
+{% block title %}
+
+
+Sobre Mim
+
+
+
+{% endblock %}
+
+{% block content %}
+<h1>Kalil Felipe Silva Fernandes
+
+</h1>
+
+
+
+<p> Eu sou o Kalil, um estudante de 17 anos, que está atualmente no 3º ano do ensino médio técnico do COTEMIG. Atuo na área de TI, com foco em criação de Softwares/Sites e Agentes de IA para automação.
+    <p>
+        Tenho experiência em HTML/CSS/JS, MySQL, C#, Python, Swagger, Swifitt + Kotlin + Dart(Com aprimoramento Flutter), N8N entre outras habilidades que você poderá ver na aba Skills, então não tenha pressa.
+
+</p>
+
+<h2> Venha conhecer mais !!! </h2>
+
+{% endblock %}
